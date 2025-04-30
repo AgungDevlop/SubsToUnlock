44ded35d8b8b21bcf6f434f54f4567be9e4d82e6
AgungDevlop <wandevprogramer@gmail.com>
2025-04-30 11:32:56 +0800

    Update Code PageLink

--- src/pages/PageLink.tsx
+++ src/pages/PageLink.tsx
@@ -15,7 +15,6 @@ const randomLinks = [
   "https://ey43.com/4/9277726",
 ];
 
-
 export function PageLink() {
   const { key } = useParams<{ key: string }>();
   const [responseData, setResponseData] = useState<any>(null);
