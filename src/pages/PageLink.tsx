b11d5dfed279700ee5b8e736c8b5c5a8d8caf4a3
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2026-08-06 03:19:48 +0800

    Update PageLink.tsx

--- src/pages/PageLink.tsx
+++ src/pages/PageLink.tsx
@@ -13,7 +13,7 @@ const API_TOKEN = "AgungDeveloper";
 
 const randomLinks = [
   "https://dulyhagglermounting.com/2082665",
-  "https://www.effectivecpmnetwork.com/b3c368xkxz?key=430d12c7fd58ecba841fd905ca2dd3fd"
+  "https://viiukuhe.com/dc/?blockID=427841"
 ];
 
 interface AdvanceOption {
