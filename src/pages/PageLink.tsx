1892b1685e8838c3aa91a4c7ae8a06c1dce16494
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2026-08-09 23:25:33 +0500

    Update PageLink.tsx

--- src/pages/PageLink.tsx
+++ src/pages/PageLink.tsx
@@ -13,7 +13,7 @@ const API_TOKEN = "AgungDeveloper";
 
 const randomLinks = [
   "https://dulyhagglermounting.com/2082665",
-  "https://www.effectivecpmnetwork.com/b3c368xkxz?key=430d12c7fd58ecba841fd905ca2dd3fd"
+  "https://sorrowfulpsychology.com/HE9TFh"
 ];
 
 interface AdvanceOption {
