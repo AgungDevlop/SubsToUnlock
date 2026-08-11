6b5e9e25745861dbf15c6ed577d8e1726d485796
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2026-08-12 02:21:58 +0500

    Update GetLink.tsx

--- src/pages/GetLink.tsx
+++ src/pages/GetLink.tsx
@@ -5,7 +5,7 @@ import { SEO } from "../components/SEO";
 
 const randomLinks = [
   "https://dulyhagglermounting.com/2082665",
-  "https://sorrowfulpsychology.com/HE9TFh"
+  "https://bmadss.com/get/?spot_id=2026313&cat=25&subid=1576043020"
 ];
 
 export function GetLink() {
